fe703e80f3cc06951f6544d71435ae2dc09f530a
shoresh57 <48243439+shoresh57@users.noreply.github.com>
2024-02-09 20:23:34 +0000

    change

--- app/frontend/src/pages/layout/Layout.tsx
+++ app/frontend/src/pages/layout/Layout.tsx
@@ -2,7 +2,6 @@
 // Licensed under the MIT license.
 
 import { Outlet, NavLink, Link } from "react-router-dom";
-
 import openai from "../../assets/openai.svg";
 import { WarningBanner } from "../../components/WarningBanner/WarningBanner";
 import styles from "./Layout.module.css";
